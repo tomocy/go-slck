1875a181ab197289956b732b7566209cbe6940bb
tomocy <tomocy.dev@gmail.com>
2020-04-15 15:54:48 +0900

    update slck command entrypoint
    SLCK command runs.

--- cmd/slck/main.go
+++ cmd/slck/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/tomocy/slck/presentation/tcp"
 )
 
-func main() {}
+func main() {
+	if err := newRunner().Run(os.Args); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
 
 func newRunner() runner {
 	return tcp.New(os.Stdout)
