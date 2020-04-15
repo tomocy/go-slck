af125ebbdc913de6a250e900a44bb6cd250687e4
tomocy <tomocy.dev@gmail.com>
2020-04-15 15:45:27 +0900

    update app
    App can listen and serve on the parsed address.

--- presentation/tcp/tcp.go
+++ presentation/tcp/tcp.go
@@ -1,9 +1,13 @@
 package tcp
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"io"
+	"net"
+
+	"github.com/tomocy/slck"
 )
 
 type app struct {
@@ -25,6 +29,31 @@ func (a *app) parseFlags(args []string) error {
 	a.addr = *addr
 
 	return nil
+}
+
+func (a app) listenAndServe(ctx context.Context) error {
+	lis, err := net.Listen("tcp", a.addr)
+	if err != nil {
+		return err
+	}
+
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+			conn, err := lis.Accept()
+			if err != nil {
+				return fmt.Errorf("failed to accept connection: %w", err)
+			}
+
+			cli := slck.NewClient(conn)
+			go cli.Listen(ctx)
+		}
+	}
 }
 
 func (a *app) printf(format string, as ...interface{}) {
