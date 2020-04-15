cc1242b25cafd4df9ccbc0f87890cbb2b50f5621
tomocy <tomocy.dev@gmail.com>
2020-04-15 15:53:56 +0900

    add newRunner

--- cmd/slck/main.go
+++ cmd/slck/main.go
@@ -1,6 +1,16 @@
 package main
 
+import (
+	"os"
+
+	"github.com/tomocy/slck/presentation/tcp"
+)
+
 func main() {}
+
+func newRunner() runner {
+	return tcp.New(os.Stdout)
+}
 
 type runner interface {
 	Run([]string) error
