334a79e3299b3f5f941cba51c836fd32aec70507
tomocy <tomocy.dev@gmail.com>
2020-04-15 15:41:00 +0900

    update app
    App can parse flags.

--- presentation/tcp/tcp.go
+++ presentation/tcp/tcp.go
@@ -1,8 +1,28 @@
 package tcp
 
-import "io"
+import (
+	"flag"
+	"fmt"
+	"io"
+)
 
 type app struct {
 	w    io.Writer
 	addr string
+}
+
+func (a *app) parseFlags(args []string) error {
+	if len(args) < 1 {
+		return fmt.Errorf("too less arguments")
+	}
+	flags := flag.NewFlagSet(args[0], flag.ContinueOnError)
+	addr := flags.String("addr", ":80", "the address to listen and serve")
+
+	if err := flags.Parse(args[1:]); err != nil {
+		return err
+	}
+
+	a.addr = *addr
+
+	return nil
 }
