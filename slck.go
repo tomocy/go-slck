e71faf8ba8322a400245710049c2a35bba849bc9
tomocy <tomocy.dev@gmail.com>
2020-04-15 16:01:49 +0900

    expose client

--- slck.go
+++ slck.go
@@ -9,7 +9,7 @@ import (
 
 type channel struct {
 	name    string
-	members map[string]client
+	members map[string]Client
 }
 
 func (c channel) broadcast(sender string, body []byte) {
@@ -19,19 +19,19 @@ func (c channel) broadcast(sender string, body []byte) {
 	}
 }
 
-func NewClient(conn net.Conn) *client {
-	return &client{
+func NewClient(conn net.Conn) *Client {
+	return &Client{
 		conn: conn,
 	}
 }
 
-type client struct {
+type Client struct {
 	conn       net.Conn
 	username   string
-	registered chan<- client
+	registered chan<- Client
 }
 
-func (c client) Listen(ctx context.Context) {
+func (c Client) Listen(ctx context.Context) {
 	for {
 		select {
 		case <-ctx.Done():
@@ -49,18 +49,18 @@ func (c client) Listen(ctx context.Context) {
 	}
 }
 
-func (c client) handle(cmd rawCommand) {
+func (c Client) handle(cmd rawCommand) {
 	switch cmd.kind {
 	default:
 		c.err(fmt.Sprintf("unknown command: %s", cmd.kind))
 	}
 }
 
-func (c client) err(msg string) {
+func (c Client) err(msg string) {
 	c.printf("%s %s\n", commandErr, msg)
 }
 
-func (c client) printf(format string, as ...interface{}) {
+func (c Client) printf(format string, as ...interface{}) {
 	fmt.Fprintf(c.conn, format, as...)
 }
 
