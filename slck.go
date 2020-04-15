3a6869a4b9ffcda7071e36ba0740763f6e0b2a31
tomocy <tomocy.dev@gmail.com>
2020-04-15 13:58:12 +0900

    add some command kinds
    CommandRegister, commandJoin, commandLeave, commandChannels, commandUsers, commandMessage, commandOK, commandErr are added.

--- slck.go
+++ slck.go
@@ -1,3 +1,14 @@
 package slck
 
 type commandKind string
+
+const (
+	commandRegister commandKind = "REGISTER"
+	commandJoin     commandKind = "JOIN"
+	commandLeave    commandKind = "LEAVE"
+	commandChannels commandKind = "CHANNELS"
+	commandUsers    commandKind = "USERS"
+	commandMessage  commandKind = "MESSAGE"
+	commandOK       commandKind = "OK"
+	commandErr      commandKind = "ERR"
+)
